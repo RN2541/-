cc157b416d72297fb9e6927a2b77862004e3bec5
Ice <icekong0906@gmail.com>
2022-08-30 13:24:36 +0700

    5cm

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,12 +14,16 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Widget myButton() {
+  Widget myButton(String buttonLabel) {
     return Expanded(
       child: OutlinedButton(
-        padding: EdgeInsets.all(24),
         onPressed: () {},
-        child: Text("XXX"),
+        child: Text(
+          buttonLabel,
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
       ),
     );
   }
@@ -34,18 +40,40 @@ class _MyAppState extends State<MyApp> {
           children: [
             Row(
               children: [
-                myButton(),
-                myButton(),
-                myButton(),
-                myButton(),
+                myButton("7"),
+                myButton("8"),
+                myButton("9"),
+                myButton("/"),
               ],
             ),
             Row(
               children: [
-                myButton(),
-                myButton(),
-                myButton(),
-                myButton(),
+                myButton("4"),
+                myButton("5"),
+                myButton("6"),
+                myButton("X"),
+              ],
+            ),
+            Row(
+              children: [
+                myButton("1"),
+                myButton("2"),
+                myButton("3"),
+                myButton("-"),
+              ],
+            ),
+            Row(
+              children: [
+                myButton("."),
+                myButton("0"),
+                myButton("00"),
+                myButton("+"),
+              ],
+            ),
+            Row(
+              children: [
+                myButton("CLEAR"),
+                myButton("="),
               ],
             ),
           ],
